2fc4d8ee149975747833d139e2b77e020400ce55
Galatia <ptrivedius@aol.com>
2021-09-22 12:02:35 -0400

    pushing current code

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -9,7 +9,25 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    price: {
+
+    },
+    stock: {
+
+    },
+    category_id: {
+
+    }
   },
   {
     sequelize,
